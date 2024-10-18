2ca704e72ff60764460832e4215cc3892875160c
Nath <115433779+Nath1710@users.noreply.github.com>
2024-10-18 05:55:59 +0000

    added TrafficLights component and the visual base for the semaphore

---
+++ src/js/component/TrafficLights.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const TrafficLights = () => {
+    return (
+        <div className='containerLights'>
+            <div className='horizontalRectangles1'></div>
+            <div className='verticalRectangle'>
+                <div className='lights'></div>
+                <div className='lights'></div>
+                <div className='lights'></div>
+            </div>
+            <div className='horizontalRectangles2'></div>
+            <div className='stick'></div>
+
+        </div>
+    )
+}
+
+export default TrafficLights
