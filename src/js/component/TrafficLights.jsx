71e7f2830d1712c58dfb60b01b467aa866f9bc53
Nath <115433779+Nath1710@users.noreply.github.com>
2024-10-18 07:15:24 +0000

    added hooks useState and useEffect for the traffic light and defines de switch case of selection

--- src/js/component/TrafficLights.jsx
+++ src/js/component/TrafficLights.jsx
@@ -1,13 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const TrafficLights = () => {
+    const [color, setColor] = useState("")
+    useEffect(() => {
+        switch (color) {
+            case "red":
+                document.querySelector(".stop").className = "lights stop selected"
+                document.querySelector(".wait").className = "lights wait"
+                document.querySelector(".go").className = "lights go"
+                break;
+
+            case "yellow":
+                document.querySelector(".wait").className = "lights wait selected"
+                document.querySelector(".stop").className = "lights stop"
+                document.querySelector(".go").className = "lights go"
+                break;
+
+            case "green":
+                document.querySelector(".go").className = "lights go selected"
+                document.querySelector(".stop").className = "lights stop"
+                document.querySelector(".wait").className = "lights wait"
+                break;
+
+        }
+    }, [color])
     return (
         <div className='containerLights'>
             <div className='horizontalRectangles1'></div>
             <div className='verticalRectangle'>
-                <div className='lights'></div>
-                <div className='lights'></div>
-                <div className='lights'></div>
+                <div className='lights stop' onClick={() => setColor("red")}></div>
+                <div className='lights wait' onClick={() => setColor("yellow")}></div>
+                <div className='lights go' onClick={() => setColor("green")}></div>
             </div>
             <div className='horizontalRectangles2'></div>
             <div className='stick'></div>
