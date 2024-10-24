c24a7e4856909f89cbcd4416f6a49ade17272b49
Nath <115433779+Nath1710@users.noreply.github.com>
2024-10-24 20:24:09 +0000

    correction using ternary operators

--- src/js/component/TrafficLights.jsx
+++ src/js/component/TrafficLights.jsx
@@ -2,41 +2,25 @@ import React, { useEffect, useState } from 'react'
 
 const TrafficLights = () => {
     const [color, setColor] = useState("")
-    useEffect(() => {
-        switch (color) {
-            case "red":
-                document.querySelector(".stop").className = "lights stop selected"
-                document.querySelector(".wait").className = "lights wait"
-                document.querySelector(".go").className = "lights go"
-                break;
 
-            case "yellow":
-                document.querySelector(".wait").className = "lights wait selected"
-                document.querySelector(".stop").className = "lights stop"
-                document.querySelector(".go").className = "lights go"
-                break;
-
-            case "green":
-                document.querySelector(".go").className = "lights go selected"
-                document.querySelector(".stop").className = "lights stop"
-                document.querySelector(".wait").className = "lights wait"
-                break;
-
-        }
-    }, [color])
     return (
         <div className='containerLights'>
-            <div className='horizontalRectangles1'></div>
+            <div className='horizontalRectangle1'></div>
             <div className='verticalRectangle'>
-                <div className='lights stop' onClick={() => setColor("red")}></div>
-                <div className='lights wait' onClick={() => setColor("yellow")}></div>
-                <div className='lights go' onClick={() => setColor("green")}></div>
+                <div className={`lights stop ${color == "red" ? "selected" : ""}`} onClick={() => setColor("red")}></div>
+                <div className={`lights wait ${color == "yellow" ? "selected" : ""}`} onClick={() => setColor("yellow")}></div>
+                <div className={`lights go ${color == "green" ? "selected" : ""}`} onClick={() => setColor("green")}></div>
             </div>
-            <div className='horizontalRectangles2'></div>
+            <div className='horizontalRectangle2'></div>
             <div className='stick'></div>
-
         </div>
     )
 }
 
 export default TrafficLights
+
+
+
+
+
+
